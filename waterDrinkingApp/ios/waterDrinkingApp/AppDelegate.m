c28f106eb137ddf47989cd80d76885568529b246
Geres99 <medryk.kontakt@gmail.com>
2021-12-21 16:34:19 +0100

    Project Init

--- waterDrinkingApp/ios/waterDrinkingApp/AppDelegate.m
+++ waterDrinkingApp/ios/waterDrinkingApp/AppDelegate.m
@@ -33,7 +33,7 @@ static void InitializeFlipper(UIApplication *application) {
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"waterDrinkingApp"
+                                                   moduleName:@"WaterDrinkingApp"
                                             initialProperties:nil];
 
   if (@available(iOS 13.0, *)) {
--- waterDrinkingApp/ios/waterDrinkingAppTests/waterDrinkingAppTests.m
+++ waterDrinkingApp/ios/waterDrinkingAppTests/waterDrinkingAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface waterDrinkingAppTests : XCTestCase
+@interface WaterDrinkingAppTests : XCTestCase
 
 @end
 
-@implementation waterDrinkingAppTests
+@implementation WaterDrinkingAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
